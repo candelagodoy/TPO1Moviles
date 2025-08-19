75896da15ca1056b00d169105bf7b1658c2338f9
Candela <candegg13@gmail.com>
2025-08-19 19:57:21 -0300

    fin

--- app/src/main/java/com/example/tpo1moviles/ModoAvionReceiver.java
+++ app/src/main/java/com/example/tpo1moviles/ModoAvionReceiver.java
@@ -3,16 +3,22 @@ package com.example.tpo1moviles;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class ModoAvionReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        boolean modoAvionDetectado = intent.getBooleanExtra("connected",false);
+        boolean modoAvionDetectado = intent.getBooleanExtra("state",false);
 
         if(modoAvionDetectado){
             Toast.makeText(context, "Modo avion activo", Toast.LENGTH_SHORT).show();
+            Intent marcando= new Intent(Intent.ACTION_DIAL);
+            marcando.setData(Uri.parse("tel:2664553747"));
+            marcando.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(marcando);
+
         }
         else{
             Toast.makeText(context, "Modo avion desactivado", Toast.LENGTH_SHORT).show();
